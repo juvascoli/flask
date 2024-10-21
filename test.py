3b939e4eed4552287dba8680b2cc6563a1860f62
juvascoli <jvascoli6@gmail.com>
2024-10-21 11:12:31 -0300

    sending a hello world message!!

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
 #pip install flask
-from flask import flask
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return{"message": "hello world!"}
+
+
+if __name__ == '__main__':
+    app.run(debug='True')
+
+
+
+
+
 
